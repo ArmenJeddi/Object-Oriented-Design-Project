2353a366e1cca1209f0e1f21cb8a1f1d7f1bb7e3
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2018-06-25 16:08:56 +0430

    ready for final phase

--- management/models/employee.py
+++ management/models/employee.py
@@ -6,7 +6,8 @@ from auth.models import User
 class Employee(User):
     asEvaluator = models.OneToOneField('Evaluator', on_delete=models.SET_NULL, null=True)
     asEvaluatee = models.OneToOneField('Evaluatee', on_delete=models.SET_NULL, null=True)
-    unit = models.CharField(max_length=20)
+    unit = models.CharField(max_length=20, null=True)
+    # user_ptr = models.OneToOneField('User', on_delete=models.CASCADE)
 
     def get_unit(self):
         return self.unit
