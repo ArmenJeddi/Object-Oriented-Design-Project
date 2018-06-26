50e1d5b8c0a9059e414d4882c2effef0ed50d9fe
ahmad nazari <nazari95ahmad@gmail.com>
2018-06-26 18:38:53 +0430

    some refactoring

---
+++ management/views/viewEvaluatorView.py
@@ -0,0 +1,17 @@
+from django.views import View
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template.loader import get_template
+
+from management.mixins import ManagerRequiredMixin
+from ..models import Employee, Evaluator
+
+
+class ViewAddEvaluatorView(ManagerRequiredMixin, View):
+
+    # GET method used for listing all evaluatee and evaluators
+    def get(self, request):
+        t = get_template('management/addEvaluator.html')
+        evaluatee = Employee.get_all_evaluatee_employee()
+        evaluators = Employee.get_all_evaluator_employee()
+        html = t.render({'evaluatee': evaluatee, 'evaluators': evaluators}, request)
+        return HttpResponse(html)
