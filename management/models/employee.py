6c3027bf19bff9bb2b3ff032cb64f332c12f7d25
ahmad nazari <nazari95ahmad@gmail.com>
2018-06-26 16:27:41 +0430

    some bugs fixed

--- management/models/employee.py
+++ management/models/employee.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 from auth.models import User
+from management.models import Evaluatee
 
 
 class Employee(User):
     # asEvaluator = models.OneToOneField('Evaluator', on_delete=models.SET_NULL, null=True)
     # asEvaluatee = models.OneToOneField('Evaluatee', on_delete=models.SET_NULL, null=True)
     unit = models.CharField(max_length=20, null=True)
+
     # user_ptr = models.OneToOneField('User', on_delete=models.CASCADE)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        as_evaluatee = Evaluatee.objects.create(asEmployee=self)
+        as_evaluatee.save()
 
     def get_unit(self):
         return self.unit
