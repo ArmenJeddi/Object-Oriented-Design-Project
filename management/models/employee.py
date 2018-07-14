685dec5da12b4efc857ffb4cb4744ed0ffc9c37d
ahmad nazari <nazari95ahmad@gmail.com>
2018-07-14 17:06:11 +0430

    view employee updated

--- management/models/employee.py
+++ management/models/employee.py
@@ -38,3 +38,12 @@ class Employee(User):
     def create(cls, username, password, name, unit):
         e = Employee(_username=username, _password=password, _name=name, _unit=unit)
         e.save()
+
+    @classmethod
+    def dump_all(cls):
+        data = []
+        for employee in cls.objects.all():
+            name = employee.get_name()
+            username = employee.get_id()
+            data.append({'name': name, 'username':username})
+        return data
--- management/views/viewEmployees.py
+++ management/views/viewEmployees.py
@@ -1,9 +1,16 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views import View
 from django.views.generic import ListView
 
 from management.models import Employee
 from management.mixins import ManagerRequiredMixin
 
-class EmployeeListView(ManagerRequiredMixin, ListView):
 
-    model = Employee
-    template_name = 'management/viewEmployees.html'
+class EmployeeListView(ManagerRequiredMixin, View):
+
+    def get(self, request):
+        employees = Employee.dump_all()
+        t = get_template('management/viewEvaluation.html')
+        html = t.render(employees, request)
+        return HttpResponse(html)
