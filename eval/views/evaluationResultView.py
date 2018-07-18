9d7c824d283f1fdfeb84a9a9320f5fea008a8e6e
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2018-07-18 11:18:00 +0430

    working on beautification

--- eval/views/evaluationResultView.py
+++ eval/views/evaluationResultView.py
@@ -11,13 +11,21 @@ class EvaluationResultView(LoginRequiredMixin, View):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.template = get_template('viewEvaluationResult.html')
+        self.user_template = get_template('viewEvaluationResult.html')
+        self.manager_template = get_template('viewEvaluationResultManager.html')
 
     def get(self, request, username):
         user = request.user
+        username = request.user.get_username()
         if user.get_job_title() == Employee.get_title():
             if user.get_username() != username:
                 return HttpResponseForbidden()
-        data = EvaluationCatalog.get_instance().dump_by_username(username)
-        html = self.template.render({'data': data}, request)
+            data = EvaluationCatalog.get_instance().dump_by_username(username)
+            html = self.user_template.render({'data': data, 'username': username}, request)
+        elif user.get_job_title() == Manager.get_title():
+            data = EvaluationCatalog.get_instance().dump_by_username(username)
+            html = self.manager_template.render({'data': data, 'username': username}, request)
+        else:
+            return HttpResponseForbidden()
+
         return HttpResponse(html)
--- management/views/viewCriteriaDetailedView.py
+++ management/views/viewCriteriaDetailedView.py
@@ -9,7 +9,6 @@ from management.models.criterion import CriterionCatalog
 class ViewCriterionDetailedView(LoginRequiredMixin, View):
     def get(self, request):
         criteria = CriterionCatalog.get_instance().dump_all()
-        print('###########', criteria)
         t = get_template('management/viewCriteriaDetailed.html')
         html = t.render({'criteria': criteria}, request)
         return HttpResponse(html)
